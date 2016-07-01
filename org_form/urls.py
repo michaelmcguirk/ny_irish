9baf86692b873817417182230e056e0e96634262
Michael McGuirk <michael.mcguirk@mydit.ie>
2016-07-01 17:30:53 +0100

    fuckin wit css

--- org_form/urls.py
+++ org_form/urls.py
@@ -4,8 +4,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'^search/', views.search, name='search'),
+	url(r'^$', views.search, name='index'),
+	url(r'^organisations/', views.search, name='search'),
 	url(r'^results/', views.results, name='results'),
 ]
 
--- org_form/views.py
+++ org_form/views.py
@@ -1,21 +1,36 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .services import org_search
+from .models import Organisation
 
 def index(request):
 	template = 'org_form/index.html'
 	return render(request, 'org_form/index.html')
 
 def search(request):
+	print('-------Search function------')
 	template = 'org_form/search.html'
-	return render(request, template)
+	org_list = Organisation.objects.all().order_by('name')
+	paginator = Paginator(org_list, 5)
+	page = request.GET.get('page')
+	try:
+		orgs = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		orgs = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		orgs = paginator.page(paginator.num_pages)
+
+	context = {'orgs' : orgs}
+	return render(request, template, context)
 
 def results(request):
+	print('-------Results function------')
 	query = request.GET['q']
 	template = 'org_form/results.html'
 	result_set = org_search(query)
-	paginator = Paginator(result_set, 10)
-
+	paginator = Paginator(result_set, 5)
 	page = request.GET.get('page')
 	try:
 		results = paginator.page(page)

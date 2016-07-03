ef4e74aa8f0db6474f3043a36fd260ac492f2b20
Michael <michael.mcguirk@mydit.ie>
2016-07-03 16:00:46 +0100

    Postgres

--- ny_irish/settings.py
+++ ny_irish/settings.py
@@ -188,6 +188,15 @@ CMS_PERMISSION = True
 CMS_PLACEHOLDER_CONF = {}
 
 DATABASES = {
+    # 'default': {
+    #     'CONN_MAX_AGE': 0,
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'HOST': 'nyirish.ctyoee9uibzm.us-west-2.rds.amazonaws.com',
+    #     'NAME': 'ny_irish',
+    #     'PASSWORD': 'Li55adel',
+    #     'PORT': '5432',
+    #     'USER': 'nyirish'
+    # }
     'default': {
         'CONN_MAX_AGE': 0,
         'ENGINE': 'django.db.backends.sqlite3',
